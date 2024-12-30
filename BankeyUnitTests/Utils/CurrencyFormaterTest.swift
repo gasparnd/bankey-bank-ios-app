23a41ef7fc796e205d9bc20fa2b121fe51160707
Gaspar Dolcemascolo <gdolcemascolo@falabella.cl>
2024-12-30 12:47:42 -0300

    renamed test class name

--- BankeyUnitTests/Utils/CurrencyFormaterTest.swift
+++ BankeyUnitTests/Utils/CurrencyFormaterTest.swift
@@ -10,7 +10,7 @@ import XCTest
 
 @testable import Bankey
 
-class Test: XCTestCase {
+class CurrencyFormatterTests: XCTestCase {
     var formatter: CurrencyFormatter!
     
     override func setUp() {
